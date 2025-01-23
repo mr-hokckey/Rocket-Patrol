fa8ee7c4bbbe843839891a705184e82a01841cfb
mr-hokckey <140927221+mr-hokckey@users.noreply.github.com>
2025-01-22 17:38:51 -0800

    Finished game!

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 let config = {
     type: Phaser.AUTO,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [ Menu, Play ]
 }
 
 let game = new Phaser.Game(config);
+
+// reserve keyboard bindings
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT;
+
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
